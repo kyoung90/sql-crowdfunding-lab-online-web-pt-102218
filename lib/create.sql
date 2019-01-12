647e2d8237d3f147ba7fb622217d32f7775671d1
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-01-12 16:00:35 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,21 @@
-CREATE TABLE 
+CREATE TABLE project(
+  id PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal TEXT,
+  start_date TEXT,
+  end_date TEXT
+);
+
+CREATE TABLE user(
+  id PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pledge(
+  id PRIMARY KEY,
+  amount REAL,
+  user_id INTEGER,
+  project_id INTEGER
+);
